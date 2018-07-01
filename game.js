f346189e746899e6b95d1b996d176afcf6ec7788
Yannis Dejonghe <yannis.dejonghe@hotmail.com>
2018-07-01 14:16:25 +0200

    added sexy emojis
    ^

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const cmds =  {
   join: join,
-  info: info
+  info: info,
+  stats: stats
 };
 let client;
 
@@ -14,8 +15,7 @@ function join(message, args) {
       message.channel.send("You've already joined in this server!");
     } else {
       Players.create({
-        player_id: message.author.id + message.guild.id,
-        gems: 500
+        player_id: message.author.id + message.guild.id
       }).then((player) => {
         message.channel.send('Welcome **' + message.author.username + '**');
       })
@@ -26,7 +26,25 @@ function join(message, args) {
 function info(message, args) {
   Players.findOne({where: {player_id: message.author.id + message.guild.id}}).then((player) => {
     if (player) {
-      message.channel.send(message.author + '\n' + player.gems + ' :gem:');
+      message.channel.send(message.author + '\n' + player.gems + ' :gem:'
+                                          + '\n' + player.combat_lvl + ' :crossed_swords: '
+                                         //place weapon here
+                                         );
+    }
+  });
+
+}
+
+function stats(message, args) {
+  Players.findOne({where: {player_id: message.author.id + message.guild.id}}).then((player) => {
+    if (player) {
+      message.channel.send(message.author + '\n' + player.combat_lvl + ' :crossed_swords: ' 
+                                          + '\n' +  player.hitpoints + ' :revolving_hearts: '
+                                          + '\n' + player.strength_lvl + ' :muscle: ' 
+                                          + '\n' + player.archery_lvl + ' :bow_and_arrow: ' 
+                                          + '\n' + player.defence_lvl + ' :shield: ' 
+                                          + '\n' + player.crafting_lvl + ' :poop:'
+                                        );
     }
   });
 
@@ -38,13 +56,13 @@ function info(message, args) {
 module.exports = (discordclient, db) => {
   Players = db.define('player', {
     player_id: Sequelize.STRING,
-    gems: Sequelize.NUMERIC,
-    hitpoints: Sequelize.NUMERIC,
-    combat_lvl: Sequelize.NUMERIC,
-    strength_lvl: Sequelize.NUMERIC,
-    archery_lvl: Sequelize.NUMERIC,
-    defence_lvl: Sequelize.NUMERIC,
-    crafting_lvl: Sequelize.NUMERIC,
+    gems: {type: Sequelize.NUMERIC, defaultValue: 500},
+    hitpoints: {type: Sequelize.NUMERIC, defaultValue: 1000},
+    combat_lvl: {type: Sequelize.NUMERIC, defaultValue: 35},
+    strength_lvl: {type: Sequelize.NUMERIC, defaultValue: 10},
+    archery_lvl: {type: Sequelize.NUMERIC, defaultValue: 10},
+    defence_lvl: {type: Sequelize.NUMERIC, defaultValue: 10},
+    crafting_lvl: {type: Sequelize.NUMERIC, defaultValue: 10},
   });
 
   client = discordclient;
