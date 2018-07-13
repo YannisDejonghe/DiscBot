d18ce14e8d3a27425ee679e0cea6b580cd2a634f
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-07-13 20:59:53 +0200

    Added basic inventory

--- game.js
+++ game.js
@@ -11,65 +11,72 @@ let client;
 
 //Database entities
 let Player;
-let PlayerWeapon;
-let Weapon;
+let PlayerItem;
 let Enemy;
+let Item;
 
 let currentEnemy;
 let currentEnemyDamages = {};
 
 function initialize() {
   let inits = {
-    weapons: [
-      {
-        name: "Shortsword",
-        mindmg: 5,
-        maxdmg: 10,
-        buy: 25,
-        sell: 10
-      },
-      {
-        name: "Evening star",
-        mindmg: 15,
-        maxdmg: 25,
-        buy: 45,
-        sell: 20
-      }
-    ],
     enemies: [
       {
         type: "Creature",
         name: "Centaur",
-        hitpoints: 1500,
         combat_lvl_min: 44,
         combat_lvl_max: 52,
-        strength_lvl: 15,
-        archery_lvl: 10,
-        defence_lvl: 15,
+        base_str: 5,
+        str: 0.2,
+        base_def: 5,
+        def: 1,
+        base_arch: 5,
+        arch: 1,
+        base_hp: 100,
+        hp: 10,
         weakness: "none"
       },
       {
         type: "Creature",
         name: "Duck",
-        hitpoints: 500,
         combat_lvl_min: 15,
         combat_lvl_max: 23,
-        strength_lvl: 10,
-        archery_lvl: 0,
-        defence_lvl: 15,
+        base_str: 3,
+        str: 1,
+        base_def: 3,
+        def: 1,
+        base_arch: 1,
+        arch: 1,
+        base_hp: 50,
+        hp: 5,
         weakness: "melee"
+      }
+    ],
+    items: [
+      {
+        name: "Shortsword",
+        buy: 25,
+        sell: 10,
+        durability: 100,
+        data: {
+          mindmg: 5,
+          maxdmg: 10
+        }
+      },
+      {
+        name: "Evening star",
+        buy: 45,
+        sell: 20,
+        durability: 100,
+        data: {
+          mindmg: 15,
+          maxdmg: 25
+        }
       }
     ]
   }
 
   let promises = [];
-
-  inits.weapons.forEach((weapon) => {
-    promises.push(Weapon.findOrCreate({
-      where: weapon,
-      defaults: weapon
-    }))
-  });
 
   inits.enemies.forEach((enemy) => {
     promises.push(Enemy.findOrCreate({
@@ -77,6 +84,14 @@ function initialize() {
       defaults: enemy
     }))
   });
+
+  inits.items.forEach((item) => {
+    promises.push(Item.findOrCreate({
+      where: item,
+      defaults: item
+    }))
+  });
+
   return Promise.all(promises);
 }
 
@@ -99,11 +114,11 @@ function playerNotRegistered(message) {
 }
 
 function info(message, args) {
-  Player.findOne({ include: [{ model: Weapon }], where: { player_id: message.author.id + ',' + message.guild.id } }).then((player) => {
+  Player.findOne({ include: [{ model: Item }], where: { player_id: message.author.id + ',' + message.guild.id } }).then((player) => {
     if (player) {
       message.channel.send(message.author + '\n' + player.gems + ' :gem:'
         + '\n' + player.combat_lvl + ' :crossed_swords: '
-        + '\n' + 'Equipped weapon: ' + player.weapons[0].name
+        + '\n' + 'Equipped weapon: ' + player.items[0].name //TODO: get equipped weapon?
       );
     } else playerNotRegistered(message);
   });
@@ -124,38 +139,47 @@ function stats(message, args) {
 }
 
 function shop(message, args) {
-  Player.findOne({ include: [{ model: Weapon }], where: { player_id: message.author.id + ',' + message.guild.id } }).then((player) => {
+  Player.findOne({ include: [{ model: Item }], where: { player_id: message.author.id + ',' + message.guild.id } }).then((player) => {
     if (player) {
-      Weapon.findAll().then(weapons => {
-        let wpns = weapons;
-        let weaponName = args.slice(1).join(' ');
+      Item.findAll().then(items => {
+        let itms = items;
+        let itemName = args.slice(1).join(' ');
 
         if (args[0] === "affordable") {
-          wpns = wpns.filter(weapon => parseInt(weapon.buy) <= parseInt(player.gems));
+          itms = itms.filter(item => parseInt(item.buy) <= parseInt(player.gems));
         }
 
-        if (args[0] === "buy" && weaponName) {
-          let weapon = wpns.find(weapon => weapon.name.toLowerCase() === weaponName.toLowerCase());
+        if (args[0] === "buy" && itemName) {
+          let item = itms.find(item => item.name.toLowerCase() === itemName.toLowerCase());
 
-          if (weapon) {
-            if (parseInt(weapon.buy) <= parseInt(player.gems)) {
-              player.gems = parseInt(player.gems) - parseInt(weapon.buy);
+          if (item) {
+            if (parseInt(item.buy) <= parseInt(player.gems)) {
+              player.gems = parseInt(player.gems) - parseInt(item.buy);
+              
+              PlayerItem.build({
+                durability: 100,
+                playerId: player.id,
+                itemId: item.id
+              }).save().then(() => {
+                message.channel.send(item.name + " bought succesfully.");
+              });
 
-              player.setWeapons([weapon]).then(() => {
+              /*player.addItem(item, {through: { durability: 100 } }).then(() => {
                 return player.save();
-              })
-                .then(() => {
-                  message.channel.send(weapon.name + " bought succesfully.");
-                });
+              }).then(() => {
+                message.channel.send(item.name + " bought succesfully.");
+              });*/
+
+              //TODO: Fix weapon equip
             } else {
-              message.channel.send("You can't afford a " + weapon.name + ".");
+              message.channel.send("You can't afford a " + item.name + ".");
             }
           } else {
-            message.channel.send("The weapon " + weapon.name + " doesn't exist.");
+            message.channel.send("The item " + item.name + " doesn't exist.");
           }
         } else {
-          wpns = wpns.map(w => w.name + ", " + w.mindmg + "-" + w.maxdmg + " dmg, buy " + w.buy + " :gem:" + " sell " + w.sell + " :gem:");
-          message.channel.send("Your funds: " + player.gems + " :gem:\n\n" + "**Weapons**\n" + wpns.join("\n"));
+          itms = itms.map(i => i.name + ", " /*+ w.mindmg + "-" + w.maxdmg + " dmg,*/ + "buy " + i.buy + " :gem:" + " sell " + i.sell + " :gem:");
+          message.channel.send("Your funds: " + player.gems + " :gem:\n\n" + "**Items**\n" + itms.join("\n"));
         }
       });
     } else playerNotRegistered(message);
@@ -164,9 +188,12 @@ function shop(message, args) {
 
 function spawn(message, args) {
   Enemy.findAll().then((enemies) => {
-    currentEnemy = enemies[0]; //todo: randomize
+    let combatlvl = parseInt(args[0]);
 
-    message.channel.send(`A ${currentEnemy.name} appeared with ${currentEnemy.hitpoints} HP!`);
+    currentEnemy = enemies[0];//[Math.floor(Math.random() * enemies.length)]; //todo: randomize
+    currentEnemy.hitpoints = currentEnemy.getStats(combatlvl).hp;
+
+    message.channel.send(`A ${currentEnemy.name} appeared with ${currentEnemy.hitpoints} HP! Stats:\n` + currentEnemy.toStatsString(combatlvl));
   });
 }
 
@@ -176,7 +203,7 @@ function attack(message, args) {
       if (player && player.weapons) {
         if (currentEnemyDamages[message.author.id]) {
           if (new Date() - currentEnemyDamages[message.author.id].lastattack < 5000) {
-            message.channel.send(`${message.author}, you're attacking too quickly!`);
+            //message.channel.send(`${message.author}, you're attacking too quickly!`);
             return;
           }
         }
@@ -229,36 +256,62 @@ module.exports = (discordclient, db) => {
     crafting_lvl: { type: Sequelize.NUMERIC, defaultValue: 10 },
   });
 
-  Weapon = db.define('weapon', {
-    name: Sequelize.STRING,
-    mindmg: Sequelize.NUMERIC,
-    maxdmg: Sequelize.NUMERIC,
-    buy: Sequelize.NUMERIC,
-    sell: Sequelize.NUMERIC
-  });
-
   Enemy = db.define('enemy', {
     type: Sequelize.STRING, // boss or creature
     name: Sequelize.STRING,
-    hitpoints: Sequelize.NUMERIC,
     combat_lvl_min: Sequelize.NUMERIC,
     combat_lvl_max: Sequelize.NUMERIC,
-    strength_lvl: Sequelize.NUMERIC,
-    archery_lvl: Sequelize.NUMERIC,
-    defence_lvl: Sequelize.NUMERIC,
+    base_str: Sequelize.NUMERIC,
+    str: Sequelize.DOUBLE,
+    base_def: Sequelize.NUMERIC,
+    def: Sequelize.DOUBLE,
+    base_arch: Sequelize.NUMERIC,
+    arch: Sequelize.DOUBLE,
+    base_hp: Sequelize.NUMERIC,
+    hp: Sequelize.DOUBLE,
     weakness: Sequelize.STRING
   });
 
-  PlayerWeapon = db.define('playerweapon', {
-    weaponId: Sequelize.NUMERIC,
-    playerId: {
-      type: Sequelize.NUMERIC,
-      unique: true
+  Item = db.define('item', {
+    name: Sequelize.STRING,
+    buy: Sequelize.NUMERIC,
+    sell: Sequelize.NUMERIC,
+    durability : Sequelize.NUMERIC,
+    data: Sequelize.JSON
+  });
+
+  Enemy.prototype.calcLinear = function(combatlvl, base, growth) {
+    return Math.floor(parseFloat(growth) * combatlvl + parseFloat(base))
+  }
+
+  Enemy.prototype.getStats = function(combatlvl) {
+    let self = this;
+    
+    return {
+      str: self.calcLinear(combatlvl, self.base_str, self.str),
+      def: self.calcLinear(combatlvl, self.base_def, self.def),
+      arch: self.calcLinear(combatlvl, self.base_arch, self.arch),
+      hp: self.calcLinear(combatlvl, self.base_hp, self.hp)
+    }
+  }
+
+  Enemy.prototype.toStatsString = function(combatlvl) {
+    let stats = this.getStats(combatlvl)
+
+    return `:muscle::skin-tone-3: ${stats.str}\n :shield: ${stats.def}\n :bow_and_arrow: ${stats.arch}\n`
+  }
+
+  PlayerItem = db.define('playeritem', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     durability: Sequelize.NUMERIC
   });
 
-  Player.belongsToMany(Weapon, { through: PlayerWeapon });
+  Player.belongsToMany(Item, { through: { model: PlayerItem, unique: false } });
+  
   //Weapon.belongsToMany(Player, {through: PlayerWeapon});
 
   client = discordclient;
